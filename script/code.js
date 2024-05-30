d1d27feca0e7e1e5d639dddebb3cde2468f6963f
AChar1f <acharif41@gmail.com>
2024-05-30 11:03:26 +0200

    updates

---
+++ script/code.js
@@ -0,0 +1,14 @@
+let cell = document.getElementsByTagName('td')
+let output1 = cell[5].innerHTML
+let output2 = cell[11].innerHTML
+let toutput = cell[14].innerHTML
+console.log(toutput)
+
+let femalequantity = document.getElementById('fquantityInput').value
+console.log(femalequantity)
+// let malequantity = document.getElementById('mquantityInput').value
+
+
+// function totalCal() {
+//     document.innerText('output').addEventListener('click', totalCal)
+// }
