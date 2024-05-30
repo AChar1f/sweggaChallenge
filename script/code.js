b5167fdd58a7b1c4d496ae9e215f90945ca7e1a3
AChar1f <acharif41@gmail.com>
2024-05-30 12:10:25 +0200

    updates

--- script/code.js
+++ script/code.js
@@ -1,15 +1,15 @@
-
 function totalCal() {
     let fPrice = document.querySelector('#fPrice').innerHTML
     let mPrice = document.querySelector('#mPrice').innerHTML
     
     let femaleQuantity = document.querySelector('#fquantityInput').value
-    let maleQuantity = document.querySelector('#mqantityInput').value
+    let maleQuantity = document.querySelector('#mquantityInput').value
     
     let fTotal = fPrice * femaleQuantity
-    let  mTotal = mPrice * maleQuantity
+    let mTotal = mPrice * maleQuantity
     let total = fTotal + mTotal
-    document.querySelector('#output1').innerHTML = fTotal
-    document.querySelector('#output2').innerHTML = mTotal
-    document.querySelector('#tOutput').innerHTML = total
+    
+    document.querySelector('#output1').innerHTML = (`R ${fTotal.toFixed(2)}`)
+    document.querySelector('#output2').innerHTML = (`R ${mTotal.toFixed(2)}`)
+    document.querySelector('#tOutput').innerHTML = (`R ${total.toFixed(2)}`)
 }
